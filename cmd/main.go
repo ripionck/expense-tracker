d64b7af0f3a54e05d2656a698e07b55beb0ceda3
ripionck <ripion.chakmar@gmail.com>
2024-12-05 16:46:21 +0600

    updated user model

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		log.Fatal("PORT environment variable not set")
+	}
+	fmt.Printf("Server starting on port: %s\n", port)
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Default())
+
+	// routes.AuthRoutes(router)
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	if err := router.Run(":" + port); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+}
--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,10 +1,20 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	ID       primitive.ObjectID `bson:" _id,omitempty"`
-	Username string             `bson:"username"`
-	Password string             `bson:"password"`
-	Email    string             `bson:"email"`
+	ID            primitive.ObjectID `bson:"_id" json:"id"`
+	Username      string             `json:"username" validate:"required,min=2,max=100"`
+	Name          string             `json:"name" validate:"required,min=2,max=100"`
+	Email         string             `json:"email" validate:"required,email"`
+	Password      string             `json:"password" validate:"required,min=6"`
+	Token         string             `json:"token,omitempty"`
+	Refresh_Token string             `json:"refresh_token,omitempty"`
+	Created_At    time.Time          `json:"created_at"`
+	Updated_At    time.Time          `json:"updated_at"`
+	User_ID       string             `json:"user_id"`
 }
