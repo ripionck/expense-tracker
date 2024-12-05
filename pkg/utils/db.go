cf9d664ae0e83f639991b8134b6195655876826c
ripionck <ripion.chakmar@gmail.com>
2024-12-05 16:29:17 +0600

    mongodb connection added

--- pkg/utils/db.go
+++ pkg/utils/db.go
@@ -1,7 +1,38 @@
 package utils
 
-// var DB *mongo.Database
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-// func ConnectDB(){
-// 	client, err := mongo.NewClient(options.Client().ApplyURI("uri string"))
-// }
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB() *mongo.Client {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loding .env file: %v", err)
+	}
+
+	uri := os.Getenv("DB_URI")
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		log.Fatal("error connecting to Mongodb:", err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal("failed to connect to mongoDB:", err)
+		return nil
+	}
+	fmt.Println("connected to DB")
+	return client
+}
+
+var Client *mongo.Client = ConnectDB()
