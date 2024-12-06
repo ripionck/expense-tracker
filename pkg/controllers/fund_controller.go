bedeba4651d4538585430f5e82d8e7e8d02ab64b
ripionck <ripion.chakmar@gmail.com>
2024-12-06 21:40:56 +0600

    added fund add function

--- pkg/controllers/fund_controller.go
+++ pkg/controllers/fund_controller.go
@@ -1,1 +1,25 @@
 package controllers
+
+import (
+	"expense-tracker/pkg/models"
+	"expense-tracker/pkg/utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AddFunds(c *gin.Context) {
+	var fund models.Fund
+	if err := c.BindJSON(&fund); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input"})
+		return
+	}
+
+	_, err := utils.FundCollection.InsertOne(c, fund)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Funds added successfully"})
+}
