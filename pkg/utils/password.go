007dbc8c6df52d266fcba0f9bbaa0c2c55ae9728
ripionck <ripion.chakmar@gmail.com>
2024-12-06 18:56:25 +0600

    added password hashing functionalities

---
+++ pkg/utils/password.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func hashedPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("failed to hash password: %w", err)
+	}
+	return string(bytes), nil
+}
+
+func VerifyPassword(hashedPassword, plainPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	if err != nil {
+		return false, "Password is incorrect."
+	}
+	return true, ""
+}
